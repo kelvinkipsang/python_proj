f2a06393cf6188938870a53182ce24c79bd664f4
kelvinkipsang <kelvinkipsang@gmail.com>
2016-08-24 19:30:10 +0300

    installing flask security and changing models

--- app.py
+++ app.py
@@ -8,21 +8,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12@localhost/test
 app.debug=True
 db=SQLAlchemy(app)
 
-class User(db.Model):
+# Define models from python hosted
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    fname = db.Column(db.String(20), unique=True)
-    sname = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
-
-    def __init__(self, fname, sname):
-        self.fname = fname
-        self.sname = sname
-
-
-
-    def __repr__(self):
-        return '<User %r>' % self.user
 
 @app.route('/')
 def index():
